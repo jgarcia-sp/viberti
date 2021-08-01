4c4078ea7d6d68264841834e96db10aeb938862d
adrianfelipeceballos <86817172+adrianfelipeceballos@users.noreply.github.com>
2021-08-01 14:22:11 +0200

    Update Ejercicio 4.py
    Cuarta iteración del ejercicio 4

--- resources/Ejercicio 4.py
+++ resources/Ejercicio 4.py
@@ -7,23 +7,23 @@ def corpora(ruta):
 #y al reconocernos los archivos dentro del ordenador metemos dentro de ruta
 #el directorio deseado, pasando primero los datos a una lista que estará en ruta
 #devolviendo finalmente los datos de esa ruta como lista.
-    """
-Con esta función ponemos la ruta del directorio como variable (ruta)
-y listas todos los documentos dentro del directorio
-"""
+#"""Con esta función ponemos la ruta del directorio como variable (ruta)
+#y listas todos los documentos dentro del directorio"""
     datos=os.listdir(ruta)
+    resultados=[]
     for ps in datos:
-        #la variable ps tiene el valor de: la iteración primera -en este momento- dentro de la lista que contiene
-        #las cadenas de los nombres de los archivos dentro del directorio alojados previamente en datos.
-        #La variable cadena contiene una cadena vacia.
-        #La variable lectura contiene nada porque no sé qué poner.
+        extensión=ps[-4:]
+        resultados.append(extensión)
+    
+    resultado=[]
+    if [".txt"] == resultados:
         lectura=open(ruta+ps,"r")
-        lectura1=lectura.read()
-        resultado=lectura1
+        lecturacompleta=lectura.read()
+        resultado=lecturacompleta        
         return resultado
         
 
 print(corpora("C:/Users/Jonathan/Desktop/viterbi/resources/Corpus/"))
 #Al declarar esto nos dará la información alojada en C en ordenador.
 
-"""print(time.time())"""
+#"""print(time.time())"""
