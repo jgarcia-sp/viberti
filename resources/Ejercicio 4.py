4c22203decb7d2b934b596a6cf9c7e82d4b6ce43
adrianfelipeceballos <86817172+adrianfelipeceballos@users.noreply.github.com>
2021-08-05 20:01:09 +0200

    Ejercicio 4 terminado
    Por fin!

--- resources/Ejercicio 4.py
+++ resources/Ejercicio 4.py
@@ -1,29 +1,20 @@
 import time
-"""print(time.time())"""
+print(time.time())
 import os
 #Se importa el módulo os que sirve para trabajr con los directorios y carpetas del ordenador.
 def corpora(ruta):
-#Esta función toma el módulo importado os
-#y al reconocernos los archivos dentro del ordenador metemos dentro de ruta
-#el directorio deseado, pasando primero los datos a una lista que estará en ruta
-#devolviendo finalmente los datos de esa ruta como lista.
-#"""Con esta función ponemos la ruta del directorio como variable (ruta)
-#y listas todos los documentos dentro del directorio"""
-    datos=os.listdir(ruta)
-    resultados=[]
-    for ps in datos:
-        extensión=ps[-4:]
-        resultados.append(extensión)
-    
-    resultado=[]
-    if [".txt"] == resultados:
-        lectura=open(ruta+ps,"r")
-        lecturacompleta=lectura.read()
-        resultado=lecturacompleta        
-        return resultado
-        
+    lista_final=[]
+    x=os.listdir(ruta)
+    lista_de_los_nombres=x
+    for nombre_fichero in lista_de_los_nombres:
+        fichero_abierto=open(ruta+nombre_fichero, "r")#open es una función
+        fichero_leido=fichero_abierto.read()
+        lista_final.append(fichero_leido)
+    return lista_final     
 
 print(corpora("C:/Users/Jonathan/Desktop/viterbi/resources/Corpus/"))
 #Al declarar esto nos dará la información alojada en C en ordenador.
 
-#"""print(time.time())"""
+print(time.time())
+
+input("Pulsa una tecla para continuar\n")
